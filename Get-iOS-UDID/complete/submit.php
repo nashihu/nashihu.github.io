59f9f6f1d3fb6865d83b1fb86c3d33a0fc273845
nashihu <ahmad.n.id@gmail.com>
2025-03-16 20:42:15 +0700

    change baseurl

--- Get-iOS-UDID/complete/submit.php
+++ Get-iOS-UDID/complete/submit.php
@@ -1,6 +1,6 @@
 <?php
 	$a = $_POST['udid'] . "\n" . $_POST['name'] . "\n" . $_POST['email'] . "\n" . $_POST['product'];
-	mail("YOU@YOUR-DOMAIN.c-o-m", "UDID", $a);
+	mail("ahmad.n.id@gmail.com", "UDID", $a);
 	header('Location: finished.html');
 ?>
 	
